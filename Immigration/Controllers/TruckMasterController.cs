a0a50dc4b1cad5e2d63be153dc8aea356ed4ab7a
DESKTOP-L51QSQ2\sarav <saravut.chanchom@gmail.com>
2023-03-07 21:57:44 +0700

    เพิ่ม Truck

---
+++ Immigration/Controllers/TruckMasterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Immigration.Controllers
+{
+    public class TruckMasterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
