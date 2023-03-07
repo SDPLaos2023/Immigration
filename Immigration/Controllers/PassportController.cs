c172c8e79646815f4952640138926c7d4d6bfd80
Ratchanon Thuknuek <ratchanon0123@gmail.com>
2023-03-07 20:58:54 +0700

    Test Commit

---
+++ Immigration/Controllers/PassportController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Immigration.Controllers
+{
+    public class PassportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PassportRegister()
+        {
+            return View();
+        }
+    }
+}
