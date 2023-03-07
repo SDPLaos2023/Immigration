1198669af18264a7c454712ac03bf80a53466d2e
Ratchanon Thuknuek <ratchanon0123@gmail.com>
2023-03-07 21:54:11 +0700

    Commit

--- Immigration/Controllers/PassportController.cs
+++ Immigration/Controllers/PassportController.cs
@@ -1,16 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Immigration.Models;
+using Immigration.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Immigration.Controllers
 {
     public class PassportController : Controller
     {
+        private PassportService oPassportService = new PassportService();
+        public PassportController() 
+        { 
+        
+        }
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
+        public IActionResult PassportRegister(long ID)
+        {
+            return View();
+        }
+
+        [HttpPost]
         public IActionResult PassportRegister()
         {
+            var _passportRegister = new PassportRegisterModel();
+            _passportRegister.Surname = "Ratchanon Thuknuek";
+            _passportRegister.GivenName = "Ratchanon Thuknuek";
+            _passportRegister.BirthOfDate = DateTime.Now.ToString("dd/MM/yyyy");
+            _passportRegister.Sex = "M";
+            _passportRegister.Country = "Thailand";
+            _passportRegister.BirthCountry = "Thailand";
+            _passportRegister.DocumentNo = 1;
+
+            //Insert PassportRegister
+            oPassportService.PassportRegisterInsert(_passportRegister);
             return View();
         }
     }
---
+++ Immigration/Models/PassportRegisterModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Immigration.Models
+{
+    public class PassportRegisterModel
+    {
+        public long Id { get; set; }
+        public string? Surname { get; set; }
+        public string? GivenName { get; set; }
+        public string? BirthOfDate { get; set; }
+        public string? Sex { get; set; }
+        public string? Country { get; set; }
+        public string? BirthCountry { get; set; }
+        public int? DocumentNo { get; set; }
+    }
+}
