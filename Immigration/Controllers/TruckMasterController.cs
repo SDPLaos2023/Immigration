b47c7ebf9924484a2efff5b3e13805383c919925
DESKTOP-L51QSQ2\sarav <saravut.chanchom@gmail.com>
2023-03-08 21:27:54 +0700

    no message

--- Immigration/Controllers/TruckMasterController.cs
+++ Immigration/Controllers/TruckMasterController.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Immigration.Models;
+using Immigration.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Immigration.Controllers
 {
     public class TruckMasterController : Controller
     {
+        private TruckMasterService oTruckMasterService = new TruckMasterService();
         public IActionResult Index()
         {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult TruckMaster()
+        {
+            var _TruckMaster = new TruckMasterModel();
+            _TruckMaster.Truck_No = "พล.80-5512";
+            _TruckMaster.Truck_Type = "6 ล้อ";
+            _TruckMaster.Truck_Name = "AA";
+
+
+            //Insert PassportRegister
+            oTruckMasterService.TruckMasterInsert(_TruckMaster);
             return View();
         }
     }
---
+++ Immigration/Services/TruckMasterService.cs
@@ -0,0 +1,57 @@
+﻿using Immigration.Entity;
+using Immigration.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace Immigration.Services
+{
+    public class TruckMasterService
+    {
+        private SDP_Immigration_DBContext _context = new SDP_Immigration_DBContext();
+        public string TruckMasterInsert(TruckMasterModel _TruckMaster)
+        {
+            try
+            {
+                var command = _context.Database.GetDbConnection().CreateCommand();
+                command.CommandText = "SP_TRUCK_MASTER_DATA_INSERT";
+                command.Parameters.Add(new SqlParameter("@TRUCK_LICENCE_PLATE_NO", _TruckMaster.Truck_No));
+                command.Parameters.Add(new SqlParameter("@TRUCK_TYPE", _TruckMaster.Truck_Type));
+                command.Parameters.Add(new SqlParameter("@COMPANY_NAME", _TruckMaster.Truck_Name));
+                
+                command.CommandType = CommandType.StoredProcedure;
+                _context.Database.OpenConnection();
+                var result = command.ExecuteScalar();
+                return "Success";
+            }
+            catch (Exception ex)
+            {
+                return "Fail";
+            }
+        }
+        
+        public DataTable TruckMasterSelect(string TruckLicencePlateNo)
+        {
+            try
+            {
+                var command = _context.Database.GetDbConnection().CreateCommand();
+                command.CommandText = "SP_TRUCK_MASTER_DATA_SELECT_BY_TRUCK_NO";
+                command.Parameters.Add(new SqlParameter("@TRUCK_LICENCE_PLATE_NO", TruckLicencePlateNo));
+                command.CommandType = CommandType.StoredProcedure;
+                _context.Database.OpenConnection();
+                var result = command.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(result);
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                DataTable dt = new DataTable();
+                return dt;
+            }
+        }
+
+    }
+
+
+}
