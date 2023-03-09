a7516b1a438be3a9b2472131513d64efabb8020d
souk <souk2400@gmail.com>
2023-03-09 19:21:46 +0700

    ::  ADD :: Passport Register page

---
+++ Immigration/Controllers/RegisterController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Immigration.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly ILogger<RegisterController> _logger;
+
+        public RegisterController(ILogger<RegisterController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Passport()
+        {
+            return View();
+        }
+    }
+}
+
+
