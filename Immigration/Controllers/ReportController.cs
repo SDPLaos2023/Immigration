4ec1eb3ba6b522ff28cd73d63b7242d241fae3a4
souk <souk2400@gmail.com>
2023-03-10 15:40:31 +0700

    :: ADD :: Checking user information page and master data report page

---
+++ Immigration/Controllers/ReportController.cs
@@ -0,0 +1,29 @@
+﻿using Immigration.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Immigration.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly ILogger<ReportController> _logger;
+
+        public ReportController(ILogger<ReportController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Mdata()
+        {
+            return View();
+        }
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+
+
